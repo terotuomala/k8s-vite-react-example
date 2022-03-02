596e431f0a2c0d527eca6d3a7f13867f13056a3a
terotuomala <tero.tuomala@protonmail.com>
2022-03-02 21:35:28 +0200

    Fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import { render } from '@testing-library/react'
 import App from './App'
 
 test('renders content', () => {
-  const component = render(
+  const { component } = render(
     <App />
   )
 
